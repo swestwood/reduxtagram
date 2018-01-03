122378fd2d0ebb3cb09b3b6fec971560d3994fbd
Sophia Westwood <sophia@quip.com>
2018-01-02 23:39:53 -0800

    Hook up Like button (#12)

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,8 +4,23 @@
 // Default params
 function posts(state = [], action) {
     // EVERY reducer runs every time there is an action!
+    // Must be a pure function -- does the same thing no matter how many
+    // times it runs on its input
     console.log("the post will change");
     console.log(state, action);
-    return state;
+    // Changes here don't get live-reloaded (only react components do)
+    // return the updated state
+    switch(action.type) {
+        case "INCREMENT_LIKES":
+            console.log("incrementing likes")
+            const i = action.index;
+            return [
+                ...state.slice(0, i),  // before the updated one
+                {...state[i], likes: state[i].likes + 1},  // update the one we want
+                ...state.slice(i + 1), // everything after
+            ]
+        default:
+            return state;
+    }
 }
 export default posts;
